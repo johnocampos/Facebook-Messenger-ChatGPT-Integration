acba89af3b2f80045942c2464eb4c8e1be007640
johnocampos <john.ocampos@victorianbeautylounge.com>
2025-04-05 16:31:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,19 @@
 module.exports = {
-    urlProfile: 'https://graph.facebook.com/v17.0/me/messenger_profile',
-    urlMesseges: 'https://graph.facebook.com/v17.0/me/messages',
-    urlAttachment: 'https://graph.facebook.com/v17.0/me/message_attachment',
-    accessToken: 'YOUR_FACEBOOK_MESSENGER_ACCESS_TOKEN',
-    postbackGetStarted: 'GET_STARTED',
-    postbackLearnMore: 'Learn More',
-    welcomeMessage: 'Welcome to Classical Art! Which artist would you like to learn more about?',
-    chatGptKey: 'YOUR_OPEN_AI_API_KEY'
-}
+  // Facebook Messenger API Endpoints
+  urlProfile: 'https://graph.facebook.com/v17.0/me/messenger_profile',
+  urlMesseges: 'https://graph.facebook.com/v17.0/me/messages',
+  urlAttachment: 'https://graph.facebook.com/v17.0/me/message_attachment',
+
+  // ✅ Your actual Page Access Token
+  accessToken: 'EAAQsM4FJkToBO2qtQqpaxUepfz974pF7YX2bnaWuui3lQB7YNCnHRD7jbLKKNfQiWYl892p2lh28yOTZBuXOcLdum55nwxvWNL8gv0Wb9Omw0f2u1uQHZB3FuQ0HsfZCCQ6a37iChxnH0FkOrE5jIfVXqKEHyny9ov70JnhCcPZBHtVoZCk2uLioxLlLvCSZCZCQvWzdbW3w9ZCeSAZDZD',
+
+  // ✅ Your actual OpenAI GPT API Key
+  chatGptKey: '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',
+
+  // Postback payloads
+  postbackGetStarted: 'GET_STARTED',
+  postbackLearnMore: 'Learn More',
+
+  // Warm, brand-aligned welcome message
+  welcomeMessage: 'Hi! Welcome to Victorian Beauty Lounge 💛 How can we help you today?'
+};
